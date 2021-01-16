d56121dfe75a9ce9b8b7742325ddab3caa22cdfb
dlepperts <danielle.leppertsimenauer@gmail.com>
2021-01-15 18:04:25 -0600

    commit tree 38b3d65ca8542768ddcde714b72c22cec8f22cc7

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,34 @@
 class FiguresController < ApplicationController
-  # add controller methods
+  
+  get '/figures' do
+    @figures = Figure.all
+    erb :'figures/index'
+  end
+
+  get '/figures/new' do
+    erb :'/figures/new'
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'/figures/show'
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    erb :'/figures/edit'
+  end
+
+  post '/figures' do
+    @figure = Figure.create(params[:figure[name]])
+    #@figure.save
+    redirect "/figures/#{@figure.id}"
+  end
+
+  patch '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    @figure.update(name: params[:name])
+    redirect "/figure/#{@figure.id}"
+  end
+
 end
--- db/migrate/01_create_landmarks.rb
+++ db/migrate/01_create_landmarks.rb
@@ -4,7 +4,6 @@ class CreateLandmarks < ActiveRecord::Migration
       t.string :name
       t.integer :figure_id
       t.integer :year_completed
-      t.timestamps null: false
     end
   end
 end
